d8b32789aaf66e0c348848e4baf85c93d5756b33
llllmithulllll <thakurmithlesh988@gmail.com>
2024-12-12 12:54:18 +0530

    Temp code

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,1 @@
-        x1 = int(CENTER[0] + mars_orbit_x[i-1] * scale)  # X-coordinate of previous point
-        y1 = int(CENTER[1] + mars_orbit_y[i-1] * scale)  # Y-coordinate of previous point
-        x2 = int(CENTER[0] + mars_orbit_x[i] * scale)    # X-coordinate of current point
-        y2 = int(CENTER[1] + mars_orbit_y[i] * scale)    # Y-coordinate of current point
-        pygame.draw.line(screen,orbit_color_mars,(x1,y1),(x2,y2),1)
+AU = 1.496e11  # 1 Astronomical Unit (meters)
